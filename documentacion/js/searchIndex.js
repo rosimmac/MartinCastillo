3d1d853aa0d46b5062e6e11f4a30a06cc6e7dd76
Rosa Maria Martin Castillo <rosamartn98@gmail.com>
2025-04-30 18:31:14 +0200

    Versión inicial del proyecto PHP documentado

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,19 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\cuadrado\u0028\u0029",
+            "name": "cuadrado",
+            "summary": "Calcula\u0020el\u0020cuadrado\u0020de\u0020un\u0020n\u00FAmero.",
+            "url": "namespaces/default.html#function_cuadrado"
+        },                {
+            "fqsen": "\\maximo\u0028\u0029",
+            "name": "maximo",
+            "summary": "Devuelve\u0020el\u0020m\u00E1ximo\u0020de\u0020dos\u0020n\u00FAmeros.",
+            "url": "namespaces/default.html#function_maximo"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
---
+++ practica-MartinCastillo.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Script de ejemplo para generar documentación con phpDocumentor.
+ *
+ * Este archivo contiene funciones matemáticas básicas con sus respectivas anotaciones de documentación.
+ *
+ * @author Martín Castillo
+ * @version 1.0
+ */
+
+/**
+ * Calcula el cuadrado de un número.
+ *
+ * Esta función recibe un número entero y devuelve su cuadrado.
+ *
+ * @param int $numero El número al que se le calculará el cuadrado.
+ * @return int El cuadrado del número.
+ *
+ * {@internal "Esta función está pensada para ser usada como base de otras operaciones."}
+ */
+function cuadrado(int $numero): int
+{
+    return $numero * $numero;
+}
+
+/**
+ * Devuelve el máximo de dos números.
+ *
+ * Compara dos enteros y devuelve el mayor.
+ *
+ * @param int $a Primer número a comparar.
+ * @param int $b Segundo número a comparar.
+ * @return int El número mayor entre $a y $b.
+ *
+ * {@internal "Este método puede mejorarse con validaciones en futuras versiones."}
+ */
+function maximo(int $a, int $b): int
+{
+    return ($a > $b) ? $a : $b;
+}
